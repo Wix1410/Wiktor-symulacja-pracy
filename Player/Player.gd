b7b2e0ba1f82b5185c6b725e5f7608a46a956e7c
Wiktor Wiktor <wiktorwix96@gmail.com>
2025-02-11 14:35:51 +0100

    zadanie9.6 poprawa struktury i początki wizualiów

--- Player/Player.gd
+++ Player/Player.gd
@@ -16,9 +16,9 @@ var knockback = Vector2.ZERO
 var stats = PlayerStats
 var reload = false
 var hitParticle = load("res://Player/PlayerHurtParticle.tscn")
-var canSprint = false
 var maxStamina = 100
 var stamina = maxStamina
+var canSprint
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
@@ -32,7 +32,6 @@ onready var light = $Light2D
 onready var sprintBar = $Sprite/SprintBar
 
 func _ready():
-	
 	stats.connect("no_health", self, "queue_free")
 	animationTree.active = true
 	animationState.start("Idle")
@@ -42,8 +41,6 @@ func _process(delta):
 	apply_central_impulse(knockback)
 	knockback = Vector2.ZERO
 	sprite.look_at(get_global_mouse_position())
-	sprintBar.visible = false
-	CanSprint(canSprint)
 	match state:
 		MOVE:
 			move_state(delta)
@@ -64,11 +61,11 @@ func move_state(delta):
 	if input_vector != Vector2.ZERO:
 		animationState.travel("walking")
 		apply_central_impulse(input_vector*90)
-		if Input.is_key_pressed(KEY_SHIFT) and canSprint:
-			animationState.travel("walking")
-			sprintBar.visible = true
+		if Input.is_key_pressed(KEY_SHIFT) and CanSprint():
 			apply_central_impulse(input_vector*90)
-			stamina =- 1
+			stamina =- sprintBar.step
+		else:
+			sprintBar.visible = false
 	else:
 		animationState.travel("Idle")
 	
@@ -138,9 +135,9 @@ func _on_Hurtbox_area_entered(area):
 	explosion.global_position = self.global_position
 	explosion.global_rotation = global_rotation
 	
-func CanSprint(canSprint):
+func CanSprint():
 	if stamina == maxStamina or stamina > 0:
 		canSprint = true
 	else:
 		canSprint = false
-	return canSprint
+
